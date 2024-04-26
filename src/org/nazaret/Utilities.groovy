c9c67d37fb82627a2594ce4dfbadda494b177ced
JG-alba <jg.alba97@gmail.com>
2024-04-26 10:42:59 +0200

    si

--- src/org/nazaret/Utilities.groovy
+++
@@ -1,7 +0,0 @@
-package org.nazaret
-
-Void printMessage(String message) {
-    println(message)
-}
-
-return this
--- vars/pipelineToChoose.groovy
+++
@@ -1,9 +0,0 @@
-def call(env){                       
-    def pipelineName;                       
-                           
-    if(env.JOB_NAME.contains('EntornoDinamico')){                  
-      pipelineName = 'entDinamico'       
-    }
-                        
-    return pipelineName       
-}
